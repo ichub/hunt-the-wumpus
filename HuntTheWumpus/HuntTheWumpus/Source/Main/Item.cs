483ca7685fd36db929dd2a1b56927d7689b08973
sviridovt <tim@tech-central.org>
2013-04-29 10:03:26 -0700

    Class declaring items

---
+++ HuntTheWumpus/HuntTheWumpus/Source/Main/Item.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HuntTheWumpus.Source.Main
+{
+    class Item
+    {
+        private List<Items> thing = new List<Items>();
+        public Item()
+        {
+            this.AddItem(1, "Bow", "Holder", true);
+            this.AddItem(2, "Arrow", "Holder", true);
+            this.AddItem(3, "Map", "Holder", false);
+            this.AddItem(4, "Standard Helmet", "Holder", true);
+            this.AddItem(5, "Standard Chest", "Holder", true);
+            this.AddItem(6, "Standard legs", "Holder", true);
+            this.AddItem(7, "Refined Helmet", "Holder", true); 
+            this.AddItem(8, "Refined Chest", "Holder", true);
+            this.AddItem(9, "Refined Legs", "Holder", true); 
+        }
+
+        public void AddItem(int decID, String decName, String decImage, bool decDamageAllowed)
+        {
+            Items it = new Items(); 
+            it.id= decID;
+            it.name= decName;
+            it.image= decImage;
+            it.damageAllowed = decDamageAllowed;
+            thing.Add(it);  
+        }
+    }
+}
