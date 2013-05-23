a93ff2df9cbdeb86289d4eb976ebad7d5282a6ae
Artem Sviridov <tim@tech-central.org>
2013-05-23 10:35:21 -0700

    instilled initialized (wasnt saved on last commit)

--- HuntTheWumpus/HuntTheWumpus/Source/Game/Derived/Misc/SpecialItems/Instilled.cs
+++ HuntTheWumpus/HuntTheWumpus/Source/Game/Derived/Misc/SpecialItems/Instilled.cs
@@ -3,9 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace HuntTheWumpus.Source.Game.Derived.Misc.SpecialItems
+namespace HuntTheWumpus.Source
 {
-    class Instilled
+    class Instilled 
     {
+        public int chance;
+        public Instilled(RingType type)
+        {
+            if (type == RingType.Standard) chance = 20;
+            else if (type == RingType.Upgraded) chance = 30;
+            else if (type == RingType.Ultimate) chance = 50;
+        }
     }
 }
